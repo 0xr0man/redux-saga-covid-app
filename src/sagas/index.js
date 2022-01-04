1eaf25bbc9d989b9ef5b31297765927cb7de92e2
Nikolai <n.darinsky@gmail.com>
2022-01-04 15:46:56 +0300

    add code review comments

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -11,7 +11,33 @@ import {
 const RAPID_API_HOST = process.env.REACT_APP_RAPID_API_HOST;
 const RAPID_API_KEY = process.env.REACT_APP_RAPID_API_KEY;
 const API_URL = process.env.REACT_APP_API_URL;
+/**
+ function* fetchResults() {
+        const json = yield call(()=> 
+        fetch(API_URL + '/countries', {
+            headers: {
+                'x-rapidapi-key': RAPID_API_KEY,
+                'x-rapidapi-host': RAPID_API_HOST,
+            },
+        })
+    )
+}
 
+function* fetchStats2() {
+    try {
+        const json = yield call(fetchResults, {})
+    } catch(e){
+        yield put(showErrorLoading(e)) // вот чтото такое тебе нужно, т.е. вызвать экшен для обработки ошибки
+    }
+    
+    yield put({ type: STATS_LOAD_SUCCESS, json: json}); 
+}
+
+
+ 1) помоему код обработки ошибок не будет работать. Ты никогда не дергаешь экшн с STATS_LOAD_ERROR
+ 2) У нас в коде этот метод разбит на куски я выше в кратце накидал как бы это у нас было написано. 
+ можешь посмотреть как используется fetchResults в results.saga.js в citeline-web
+ */
 function* fetchStats() {
     const json = yield call(()=> 
         fetch(API_URL + '/countries', {
